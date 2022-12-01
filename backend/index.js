3b7b12473c5e81e33d22c0aed2d85bceb348cd2c
Benjamin Boullé <bohuny@hotmail.com>
2022-12-01 21:47:49 +0100

    feat: af,kezgrgnkgnree

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,25 @@ import 'dotenv/config'
 import UsersService from './services/UsersService.js'
 
 import { MongoClient } from 'mongodb'
+import NotifsRoutes from './routes/NotifsRoutes.js'
 
 const uri ="mongodb://20.111.50.245:27017/"
 const client = new MongoClient(uri)
 
 const services = {
-    "users" : new UsersService(client)
+    "users": new UsersService(client),
+    "notifs": new NotifsRoutes(client)
 }
 
 const app = express()
 const port = process.env.PORT
 app.use(cors())
+app.use(express.json())
 
 const routes = getRoutes(services)
 
 for (const routeName in routes) {
-    const route = routes[routName]
+    const route = routes[routeName]
     if(route[0] === "GET"){
       app.get('/' + routeName, (req, res) => {
         route[1](req, res);
