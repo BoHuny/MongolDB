6ff6d0c9d48f6ced0685f1b6da2f6d46360245eb
Mathias COUSSAT <mathias.coussat@free.fr>
2022-12-01 23:40:38 +0100

    sjkiuhjji

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,8 @@ import UsersService from './services/UsersService.js'
 import NotifsService from './services/NotifsService.js'
 
 import { MongoClient, ObjectId } from 'mongodb'
-<<<<<<< HEAD
 import Notif from './model/Notif.js'
-=======
 import { authenticateToken, generateAccessToken } from './utils/jwt.js'
->>>>>>> 5d0156f64f7d04237b21311e6da11120073af447
 
 const uri ="mongodb://20.111.50.245:27017/"
 const database = new MongoClient(uri).db("mongolDB")
@@ -28,7 +25,7 @@ const services = {
 
 // await notifService.createNotifForOneUser("63891a76136dd44526e03e84", new Notif("BZ BZ BZ BZ BZ BZ", 2, "WASSIMOUNET", 3))
 
-let result = await notifService.readAllNotifs("63891a76136dd44526e03e84")
+// let result = await notifService.readAllNotifs("63891a76136dd44526e03e84")
 // console.log(result)
 
 const app = express()
@@ -38,24 +35,12 @@ app.use(express.json())
 
 const routes = getRoutes(services)
 
-console.log(generateAccessToken("ABV"))
-
 for (let i = 0; i < routes.length; i++) {
   const route = routes[i]
-<<<<<<< HEAD
-    if (route.needAuthent) {
-      // TODO Authent
-    } 
-    if(route.method === "GET"){
-      app.get('/' + route.path, (req, res) => {
-        route.callback(req, res)
-      })
-=======
     if(route.method === "GET") {
         app.get('/' + route.path, (req, res, next) => authenticateToken(route.needAuthent, req, res, next), (req, res) => {
           route.callback(req, res)
         })
->>>>>>> 5d0156f64f7d04237b21311e6da11120073af447
     }
     else {
         app.post('/' + route.path, (req, res, next) => authenticateToken(route.needAuthent, req, res, next), (req, res) => {
