c31a06663bab5c30b5a104be2dc406fef99e1a0e
Mathias COUSSAT <mathias.coussat@free.fr>
2022-12-01 21:59:56 +0100

    add super

--- backend/model/Notif.js
+++ backend/model/Notif.js
@@ -2,6 +2,7 @@ import MongoDBEntity from './MongoDBEntity'
 
 export default class Notif extends MongoDBEntity{
     constructor(title, type, description, severity, isRead = false, date = null, id = null) {
+        super(id)
         if(date === null){
             this.date = new Date()
         } else {
