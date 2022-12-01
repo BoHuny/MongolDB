30f5ef8286e2e5f3c252aee2f5df848c653234d7
Mathias COUSSAT <mathias.coussat@free.fr>
2022-12-01 22:10:27 +0100

    notifs service

--- backend/services/NotifsService.js
+++ backend/services/NotifsService.js
@@ -3,11 +3,26 @@ import User from "../model/Notif.js"
 export default class NotifsService {
     constructor(client) {
         this.client = client
-        this.notifsCollection = client.collection('notifs');
+        this.notifsCollection = client.collection('notifs')
+        this.usersCollection = client.collection('users')
     }
 
     createNotifs(notif) {
         let result = this.notifsCollection.insertOne(notif);
         return result
+    }
+
+    async getNotifs(idUser, isRead) {
+        const queryUser = { _id: idUser}
+        const optionUser = {}
+        let user = await this.usersCollection.findOne(queryUser, optionUser);
+        notifsIds = user.listNotifs
+        allNotifs = []
+        notifsIds.forEach(idNotif => {
+            const queryNotif = {_id: idNotif}
+            const optionNotif = {}
+            allNotifs.push(this.notifsCollection.findOne(queryNotif, optionNotif))
+        });
+        return allNotifs
     }
 }
