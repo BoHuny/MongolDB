c398e88173ed0838f99f5825ed2e9334a106d831
antadama <antaadama@gmail.com>
2022-12-02 07:43:05 +0100

    ffff

--- public/script.js
+++ public/script.js
@@ -149,20 +149,25 @@ function getUserScore(){
 
 
 function getUserNotifs(){
-    console.log("IN");
-    var output = ""; 
     $.ajax({
         contentType: 'application/json',
         dataType:'json',
+        data: JSON.stringify(
+            {
+                sendOnlyUnread:false,
+            }),
         success:function(data){
             console.log("Success");
+            console.log(data);
             const notifs = data;
             let list = document.getElementById("myNotifList");
+            let message= "";
             notifs.forEach((item) => {
                 let div = document.createElement("div");
                 let title = item.title;
                 let pseudo = item.data.pseudo;
                 let isProtected = item.data.isProtected;
+                let notifId=item._id;
 
                 if (isProtected){
                     var message = pseudo+ " veut coucher avec toi avec une protection ;)\n";
@@ -171,30 +176,61 @@ function getUserNotifs(){
                     var message = pseudo+ " veut coucher avec toi sans protection... C'est plus excitant !\n";
                 }
                 console.log(message);
-                output += message;
 
-                /*let bloc = document.createElement("p");
+                let bloc = document.createElement("p");
                 let next_line = document.createElement("br");
                 let t = document.createElement("b"); 
                 t.textContent = title;
                 bloc.appendChild(t);
                 bloc.append(next_line);
                 bloc.append(message);
+
                 div.appendChild(bloc);
-                list.appendChild(div);*/
+
+                let button1 = document.createElement("button");
+                let button2 = document.createElement("button");
+                button1.id= "acceptToF";
+                button2.id= "refuseToF";
+                let personId = item._id;
+                button1.setAttribute('onclick',"respondtoF('"+notifId+"','true','"+button1.id+"')");
+                button2.setAttribute('onclick',"respondtoF('"+notifId+"','false','"+button2.id+"')");
+                button1.textContent="Accepter le rapport";
+                button2.textContent="Refuser le rapport";
+                div.appendChild(button1);
+                div.appendChild(button2);
+                list.appendChild(div);
             });
         },
         error:function(){
             console.log("error")
         },
-        type: 'GET',
+        type: 'POST',
         url:"http://localhost/getNotifs"
     })
-    if (output===""){
-        return "Aucune notif..."
-    }
-    else{
-        return output;
-    }
 }
 
+function respondtoF (idNotif,isAccepted, idButton) {
+
+    document.getElementById(idButton).parentElement.innerHTML = "";
+
+
+    $.ajax({
+        contentType: 'application/json',
+        data: JSON.stringify(
+            {
+                idNotif:idNotif,
+                response:isAccepted
+            }
+        ),
+        dataType: 'json',
+        success: function(data){
+            console.log("Success");
+        },
+        error: function(){
+            console.log("error")
+        },
+        type: 'POST',
+        url: 'http://localhost/respondToF'
+    })
+
+}
